125faceed381dd4479c65bb9797802852ff46f56
Ibrokhimjon <Abiturientlaruchun@gmail.com>
2021-10-11 17:49:08 +0500

    initial files

---
+++ alfamed/resources/views/components/base.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="description" content="Orbitor,business,company,agency,modern,bootstrap4,tech,software">
+    <meta name="author" content="themefisher.com">
+
+    <title>Novena- Health & Care Medical template</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/favicon.ico')}}" />
+
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="{{asset("plugins/bootstrap/css/bootstrap.min.css")}}">
+    <!-- Icon Font Css -->
+    <link rel="stylesheet" href="{{asset("plugins/icofont/icofont.min.css")}}">
+    <!-- Slick Slider  CSS -->
+    <link rel="stylesheet" href="{{asset("plugins/slick-carousel/slick/slick.css")}}">
+    <link rel="stylesheet" href="{{asset("plugins/slick-carousel/slick/slick-theme.css")}}">
+
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="{{asset("css/style.css")}}">
+
+</head>
+
+<!-- Main jQuery -->
+<script src="{{asset("plugins/jquery/jquery.js")}}"></script>
+<!-- Bootstrap 4.3.2 -->
+<script src="{{asset("plugins/bootstrap/js/popper.js")}}"></script>
+<script src="{{asset("plugins/bootstrap/js/bootstrap.min.js")}}"></script>
+<script src="{{asset("plugins/counterup/jquery.easing.js")}}"></script>
+<!-- Slick Slider -->
+<script src="{{asset("plugins/slick-carousel/slick/slick.min.js")}}"></script>
+<!-- Counterup -->
+<script src="{{asset("plugins/counterup/jquery.waypoints.min.js")}}"></script>
+
+<script src="{{asset("plugins/shuffle/shuffle.min.js")}}"></script>
+<script src="{{asset("plugins/counterup/jquery.counterup.min.js")}}"></script>
+<!-- Google Map -->
+<script src="{{asset("plugins/google-map/map.js")}}"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeLMlsiwzp6b3Gnaxd86lvakimwGA6UA&callback=initMap"></script>
+
+<script src="{{asset("js/script.js")}}"></script>
+<script src="{{asset("js/contact.js")}}"></script>
+
+</body>
+</html>
